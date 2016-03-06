4f2731bb4a84254005cc2d92143762de95048ea4
Oded Leiba <oded@colu.co>
2016-03-06 17:07:40 +0200

    refine /assetmetadata path

--- controllers/v2/color.js
+++ controllers/v2/color.js
@@ -97,13 +97,13 @@ module.exports = (function () {
         var getAssetMetadata = {
             'spec': {
                 "description": "",
-                "path": "/assetmetadata/{assetId}/{utxo}?",
+                "path": "/assetmetadata/{assetId}/{utxo}?/?verbosity={verbosity}",
                 "notes": "Returns information about an asset issuence",
                 "summary": "",
                 "method": "GET",
                 "parameters": [
                     sw.pathParam("assetId", "ID of Asset we want to get info for", "string", true),
-                    sw.pathParam("utxo", "provide data for secific utxo", "string", true),
+                    sw.pathParam("utxo", "provide data for secific utxo", "string", false),
                     sw.formParam("verbosity", "how detailed is the metadata, varying from 0 to 2 where 0 is the most basic", "string", false)
                 ],
                 "type": "assetMetadataResponse",
@@ -111,6 +111,7 @@ module.exports = (function () {
                 "nickname": "getAssetMetadata"
             },
             'action': function (req, res) {
+                console.log('req.params.verbosity = ', req.params.verbosity)
                 var verbosity = parseInt(req.params.verbosity)
                 verbosity = ([0,1,2].indexOf(verbosity) > -1)? verbosity : 1
                 api.getAssetMetadata(req.params.assetId, req.params.utxo, verbosity).
